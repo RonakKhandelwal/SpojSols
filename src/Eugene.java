8c5d42ba83859a97dcf277240a9d08d025dfbd38
CryNoz <ronak.khandelwal@learner.manipal.edu>
2015-11-11 19:11:34 +0530

    11/11/15

--- src/Eugene.java
+++ src/Eugene.java
@@ -13,15 +13,16 @@ public class Eugene {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t;
         t = Integer.parseInt(br.readLine());
-        int ans[] = new int[t];
+
+        long ans[] = new long[t];
         int i = 0;
         while (i < t) {
             String test = br.readLine();
             StringTokenizer st = new StringTokenizer(test);
-            int val[] = new int[4];
+            long val[] = new long[4];
             int w = 0;
             while (st.hasMoreTokens()) {
-                val[w++] = Integer.parseInt(st.nextToken());
+                val[w++] = Long.parseLong(st.nextToken());
             }
             ans[i++] = apFx(val[0], val[1], val[2], val[3]);
         }
@@ -29,45 +30,81 @@ public class Eugene {
         for (int j = 0; j < ans.length; j++) {
             System.out.println(ans[j]);
         }
-      /*  for (int i = 1; i < 10; i++) {
-            for (int j = 1; j < 10; j++) {
-                for (int k = 1; k < 10; k++) {
-                    for (int l = 1; l < k; l++) {
-                        System.out.println(i + " " + j + " " + l + " " + k + " ans  " + apFx(i, j, l, k));
+      /*  for (long i = 1; i < 10; i++) {
+            for (long j = 1; j < 10; j++) {
+                for (long k = 1; k < 10; k++) {
+                    for (long l = 1; l < k; l++) {
+                        System.out.prlongln(i + " " + j + " " + l + " " + k + " ans  " + apFx(i, j, l, k));
                     }
                 }
             }
         }*/
     }
 
-    private static int apFx(int a, int d, int l, int r) {
-        int sum = 0;
-        int add = 1;
-        int num = a + (l - 1) * d;
+    private static long apFx(long a, long d, long l, long r) {
+        long sum = 0;
+        long add ;
+        long num = a + (l - 1) * d;
         add = f(num);
-        for (int i = l; i <= r; i++) {
+
+        if (d % 9 == 0) {
+            long fac = r - l + 1;
+            sum = add * fac;
+            return sum;
+
+        } else {
+
+           // long w = d % 9;
+            long x = r - l +1;
+            if (x % 9 == 0) {
+                x = x/9;
+                sum = 45 * x;
+                return sum;
+            } else {
+                long k = x % 9;
+                while (k > 0) {
+                    sum = sum + add;
+                    add += d;
+                    if (add > 9) {
+                        add = add % 9;
+                    }
+                    if (add == 0) {
+                        add = 9;
+                    }
+                    k--;
+                }
+
+                long fact = x / 9;
+                long ans = sum + fact * 45;
+                return ans;
+
+            }
+        }
+        /*for (long i = l; i <= r; i++) {
 
             sum = sum + add;
             add += d;
             if (add > 9) {
                 add = add % 9;
             }
+            if (add == 0){
+                add = 9;
+            }
         }
 
 
-        return sum;
+        return sum;*/
     }
 
-    public static int f(int x) {
+    public static long f(long x) {
         if (x < 10)
             return x;
         else {
-            int sum = 0;
-            while (x != 0) {
-                sum = sum + x % 10;
-                x = x / 10;
+            long ans = x % 9;
+            if (ans == 0) {
+                ans = 9;
             }
-            return f(sum);
+            return ans;
 
         }
     }
--- src/Test.java
+++ src/Test.java
@@ -1,27 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 /**
- * Created by ronak on 9/11/15.
+ * Created by ronak on 6/11/15.
  */
 public class Test {
 
-    public static void main(String[] args) {
-        for (int i = 15; i < 30; i++) {
-            System.out.println(i+" "+f(i));
-        }
-    }
 
-    public static int f(int x) {
-        if (x < 10)
-            return x;
-        else {
-            int sum = 0;
-            while (x != 0) {
-                sum = sum + x % 10;
-                x = x / 10;
+    public static void main(String[] args) throws IOException {
+
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 30; j++) {
+                for (int k = 1; k < 100; k++) {
+                    for (int l = 0; l < k; l++) {
+                        System.out.println(i + " " + j + " " + l + " " + k + " " + apFx(i, j, l, k));
+                    }
+                }
             }
-            return f(sum);
-
         }
+
     }
 
+    private static int apFx(int a, int d, int l, int r) {
+        int sum = 0;
+
+        for (int i = l; i <= r; i++) {
+            int num = a + (i - 1) * d;
+            int finalSum = num;
+            while (finalSum > 10) {
+                int temp = 0;
+                while (num != 0) {
+                    temp = temp + num % 10;
+                    num = num / 10;
+                }
+                if (temp < 10) {
+                    finalSum = temp;
+                } else num = temp;
+            }
+            sum += finalSum;
+
+
+        }
+
+
+        return sum;
+    }
 
 }
+     
